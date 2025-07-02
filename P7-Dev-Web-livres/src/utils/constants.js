bcf129f701f12dc98900cbba828826f642ecab3f
Nityy20 <vincent-2905@hotmail.Fr>
2025-07-02 02:12:48 +0200

    correction

---
+++ P7-Dev-Web-livres/src/utils/constants.js
@@ -0,0 +1,16 @@
+const API_URL = 'http://localhost:3000/api';
+
+export const API_ROUTES = {
+  SIGN_UP: `${API_URL}/auth/signup`,
+  SIGN_IN: `${API_URL}/auth/login`,
+  BOOKS: `${API_URL}/books`,
+  BEST_RATED: `${API_URL}/books/bestrating`,
+};
+
+export const APP_ROUTES = {
+  SIGN_UP: '/Inscription',
+  SIGN_IN: '/Connexion',
+  ADD_BOOK: '/Ajouter',
+  BOOK: '/livre/:id',
+  UPDATE_BOOK: 'livre/modifier/:id',
+};
